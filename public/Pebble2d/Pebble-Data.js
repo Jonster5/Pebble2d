71353011e0fc4609f3942b99a7bd2bc18ae59f91
Jonster5 <onlyaccelerating@gmail.com>
2019-11-12 03:35:53 -0500

    datasaver

--- public/Pebble2d/Pebble-Data.js
+++ public/Pebble2d/Pebble-Data.js
@@ -1,27 +1,58 @@
 Pebble.Data = class {
     constructor() {
         this.databases = [];
-        this.openDBs = [];
+        this.openDatabases = [];
+
+        for (let i = 0; i < window.localStorage.length; i++) {
+            let key = window.localStorage.key(i);
+            let foo = window.localStorage.getItem(key);
+            foo = JSON.parse(foo);
+            if (typeof(foo.id) === "string") this.databases.push(foo);
+        }
     }
     createDB(name = "") {
+        let good;
         this.databases.forEach(db => {
-            if (db.name === name) {
-                throw new Error("This name has already been chosen");
+            if (db.id === name) {
+                good = false;
             }
         });
-        let db = { id: name, data: {} };
-        this.databases.push(db);
-        window.localStorage.setItem(name, JSON.stringify(db));
+        if (good === true) {
+            let db = { id: name, data: {} };
+            this.databases.push(db);
+            window.localStorage.setItem(name, JSON.stringify(db));
+            return true;
+        } else {
+            return false;
+        }
+    }
+    removeDB(name = "") {
+        let db = this.databases.find(x => x.id === name);
+        let index = this.databases.indexOf(db);
+        window.localStorage.removeItem(db.id);
+        this.databases.splice(index, 1);
         return true;
     }
     open(database = "", callback) {
         let db = JSON.parse(window.localStorage.getItem(database));
-        this.openDBs.push(db);
-        let index = this.openDBs.indexOf(db);
-        callback(db.id, this.openDBs[index].data);
+        this.openDatabases.push(db);
+        let index = this.openDatabases.indexOf(db);
+        callback(this.openDatabases[index].data);
+        return true;
     }
     close(database = "") {
-        let db = this.openDBs.find(x => x.id === database);
+        let db = this.openDatabases.find(x => x.id === database);
         window.localStorage.setItem(db.id, JSON.stringify(db));
+        let index = this.openDatabases.indexOf(db);
+        this.openDatabases.splice(index, 1);
+        return true;
+    }
+    removeAll() {
+        this.databases.forEach(db => this.removeDB(db.id));
+        return true;
+    }
+    closeAll() {
+        this.openDatabases.forEach(db => this.close(db.id));
+        return true;
     }
 }
