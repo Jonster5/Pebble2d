74d3293580d3d79ce98f29e2c906e2ae30a851cd
Jonster5 <onlyaccelerating@gmail.com>
2019-11-18 18:42:08 +0000

    yes6

--- public/main-3.js
+++ public/main-3.js
@@ -1,4 +1,4 @@
-let canvas = new Pebble.Canvas(document.body, 512, 512, "1px solid black");
+let canvas = new Pebble.Canvas(document.body, 512, 512, "5px solid black");
 let stage = new Pebble.Stage(canvas.width, canvas.height);
 let assets = new Pebble.AssetLoader();
 
@@ -16,6 +16,12 @@ let up = Pebble.Keyboard(38),
     right = Pebble.Keyboard(39),
     down = Pebble.Keyboard(40),
     left = Pebble.Keyboard(37);
+
+let w = Pebble.Keyboard(87),
+    a = Pebble.Keyboard(65),
+    s = Pebble.Keyboard(83),
+    d = Pebble.Keyboard(68);
+
 
 Pebble.interpolationData.FPS = 30;
 
@@ -87,37 +93,45 @@ function setup() {
     gameOverScene.visible = false;
 
     left.press = () => {
-        if (right.isUp) player.vx = -5;
+        if (right.isUp) player.vx = -3;
         else player.vx = 0;
     };
     left.release = () => {
         if (right.isUp) player.vx = 0;
-        else player.vx = 5;
+        else player.vx = 3;
     };
     right.press = () => {
-        if (left.isUp) player.vx = 5;
+        if (left.isUp) player.vx = 3;
         else player.vx = 0;
     };
     right.release = () => {
         if (left.isUp) player.vx = 0;
-        else player.vx = -5;
+        else player.vx = -3;
     };
     up.press = () => {
-        if (down.isUp) player.vy = -5;
+        if (down.isUp) player.vy = -3;
         else player.vy = 0;
     };
     up.release = () => {
         if (down.isUp) player.vy = 0;
-        else player.vy = 5;
+        else player.vy = 3;
     };
     down.press = () => {
-        if (up.isUp) player.vy = 5;
+        if (up.isUp) player.vy = 3;
         else player.vy = 0;
     };
     down.release = () => {
         if (up.isUp) player.vy = 0;
-        else player.vy = -5;
+        else player.vy = -3;
     };
+    w.press = () => up.press();
+    w.release = () => up.release();
+    a.press = () => left.press();
+    a.release = () => left.release();
+    s.press = () => down.press();
+    s.release = () => down.release();
+    d.press = () => right.press();
+    d.release = () => right.release();
 
     Animate();
 }
@@ -134,15 +148,15 @@ function update() {
 
     Pebble.contain(player, {
         x: 32,
-        y: 32,
+        y: 16,  
         width: canvas.width - 32,
         height: canvas.height - 32
     });
 
-    enemies.forEach(enemy => {
+    for (i in enemies) {
+        let enemy = enemies[i];
         enemy.x += enemy.vx;
         enemy.y += enemy.vy;
-        let playerHit;
 
         Pebble.contain(enemy, {
             x: 32,
@@ -150,19 +164,32 @@ function update() {
             width: canvas.width - 32,
             height: canvas.height - 32
         }, true);
-
-
+       
         if (Pebble.hit(player, enemy)) {
-            playerHit = true;
-        }
-        if (playerHit) {
-            player.alpha = 0.5;
-            healthBar.inner.width -= 1;
+            player.blendMode = "lighter";
+            healthBar.inner.width -= 5;
+            break;
         } else {
-            player.alpha = 1;
+            player.blendMode = "none";
         }
-    });
+    }
 
+    if (Pebble.hit(player, treasure)) {
+        treasure.x = player.x + 8;
+        treasure.y = player.y + 8;
+    }
 
+    if (Pebble.hit(treasure, exit)) {
+        gameScene.visible = false;
+        gameOverScene.visible = true;
+        stage.putCenter(message);
+        message.content = "You Won!";
+    }
 
-}
+    if (healthBar.inner.width <= 0) {
+        gameScene.visible = false;
+        gameOverScene.visible = true;
+        stage.putCenter(message);
+        message.content = "You Lost!";
+    }h
+};
